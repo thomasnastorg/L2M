4e8d6f112a34fe9946ffe5ce611a4b199083c3b0
HUET Dolores <dolores.hv3t@gmail.com>
2021-04-26 22:30:15 +0200

    test

---
+++ Locaux.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center><h1> LOCAUX DE LA M2L </h1></center>
+
+<p>
+Le site se compose de quatre bâtiments, dont deux dotés de quatre étages et donc de 5 niveaux (les bâtiments 1 et C) et deux de plain-pied (les bâtiments B et D) dotés d'un seul rez-de-chaussée. Les bâtiments C et D sont neufs.
+Les bâtiments A et B datent d'une quinzaine d'années.
+
+Les étages des bâtiments A et C hébergent les bureaux des ligues locataires et le rez-de-chaussée des quatres bâtiments héberge des espaces mutualisés : un amphithéâtre de 20 places avec une régie, 8 salles de réunion de 12 à 50 places, un hall d'accueil, une salle de convivialité et son office traiteur, ainsi qu'une salle de formation multimédia dotée de 24 postes. on y trouve également la partie "accueil" des neuf bureaux de l'administration de la M2L. L'autre partie se trouve au premier étage du bâtiment C. Différents locaux de service (archives, stockage local d'entretien etc...) se trouvent en sous-sol. 
+
+En fonction de leur taille, les 24 structures hébergées occupent un ou plusieurs bureaux. Au total, la M2L offre 80 bureaux. À chaque étage des bâtiments de bureaux se trouve une petite salle de réunion que les ligues utilisent sasn réservation. L'ensemble des autres salles ressources sont accessibles sur réservation, en journée comme en soirée.
+
+<h2> IPLANTATION DES QUATRES BÂTIMENTS<h2>
+
+<a href=
+
+</p>
+</body>
+</html>
