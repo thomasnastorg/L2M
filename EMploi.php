4e085accada15994ec8b5aa508d52712c11511ee
ThouveninJules <thouveninjules@gmail.com>
2021-04-26 20:01:27 +0200

    Update EMploi.php

--- EMploi.php
+++ EMploi.php
@@ -3,6 +3,8 @@
 <h1>Rechercher une offre</h1>
 
 <input type="radio" name="Categ" id="1">Recherche par catégorie</input>
+<h3>Je recherche</h3>
+<input:select>Un emploi</input:select>
 
     
 </body>
