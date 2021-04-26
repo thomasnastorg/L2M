e662563a26bbe9e4872e3ef179541544d4272bf7
ThouveninJules <thouveninjules@gmail.com>
2021-04-26 20:44:38 +0200

    Update EMploi.php

--- EMploi.php
+++ EMploi.php
@@ -4,7 +4,39 @@
 
 <input type="radio" name="Categ" id="1">Recherche par catégorie</input>
 <h3>Je recherche</h3>
-<input:select>Un emploi</input:select>
+<select name="type" id="type-rech">
+    <option value="">-Veuillez choisir une option-</option>
+    <option value="">CDI</option>
+    <option value="">CDD</option>
+    <option value="">Stage</option>
+    <option value="">Alternance</option>
+</select>
+<h3>Dans le domaine</h3>
+<select name="domaine" id="domaine">
+    <option value="">-Veuillez choisir une option-</option>
+    <option value="">Informatique</option>
+    <option value="">Comptabilité</option>
+    <option value="">Managérial</option>
+    <option value="">Autre</option>
+</select>
+<br><br>
+<h3>En</h3>
+<select name="domaine" id="domaine">
+    <option value="">-Veuillez choisir une option-</option>
+    <option value="">Auvergne-Rhône-Alpes</option>
+    <option value="">Bourgogne-Franche-Comté</option>
+    <option value="">Bretagne</option>
+    <option value="">Centre-Val de Loire</option>
+    <option value="">Corse</option>
+    <option value="">Grand Est</option>
+    <option value="">Hauts-de-France</option>
+    <option value="">Île-de-France</option>
+    <option value="">Normandie</option>
+    <option value="">Nouvelle-Aquitaine</option>
+    <option value="">Occitanie</option>
+    <option value="">Pays de la Loire</option>
+    <option value="">Provence-Alpes-Côte d'Azur</option>
+</select>
 
     
 </body>
