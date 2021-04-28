500e97e9367f491f61cb48de9d31a29253998c1a
ThouveninJules <thouveninjules@gmail.com>
2021-04-28 22:23:08 +0200

    Update Emploi.php

--- Emploi.php
+++ Emploi.php
@@ -38,20 +38,8 @@ Dans le domaine
 En
 <br><br>
 <select name="Lieu" id="Lieu">
-    <option value="France">-Veuillez choisir une option-</option>
-    <option value="Auvergne-Rhône-Alpes">Auvergne-Rhône-Alpes</option>
-    <option value="Bourgogne-Franche-Comté">Bourgogne-Franche-Comté</option>
-    <option value="Bretagne">Bretagne</option>
-    <option value="Centre-Val de Loire">Centre-Val de Loire</option>
-    <option value="Corse">Corse</option>
-    <option value="Grand Est">Grand Est</option>
-    <option value="Hauts-de-France">Hauts-de-France</option>
-    <option value="Île-de-France">Île-de-France</option>
-    <option value="Normandie">Normandie</option>
-    <option value="Nouvelle-Aquitaine">Nouvelle-Aquitaine</option>
-    <option value="Occitanie">Occitanie</option>
-    <option value="Pays de la Loire">Pays de la Loire</option>
-    <option value="Provence-Alpes-Côte d'Azur">Provence-Alpes-Côte d'Azur</option>
+    <option value="Lorraine">-Veuillez choisir une option-</option>
+    <option value="Lorraine">Lorraine</option>
 </select>
 <br><br><br><br>
 <input type="radio" name="Rech" id="cle">Recherche par nom ou référence</input>
