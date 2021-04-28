0c5b179598a9656d520fb9af424491b0f3c31616
HUET Dolores <dolores.hv3t@gmail.com>
2021-04-28 21:53:08 +0200

    juridique

---
+++ statutJuridique.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center><h1> STATUT JURIDIQUE </h1></center>
+
+<p>
+La Maison des Ligues est un établissement du Conseil Régional. Ce n’est pas une entité juridique en propre.
+Elle est financée à 100 % (pour son fonctionnement et pour la construction récente de l’extension des bâtiments C et D) par le Conseil Régional et sans aucune participation du Conseil Général de Meurthe et Moselle, bien qu’elle abrite un certain nombre de comités départementaux.
+Une convention de cogestion a été passée entre le Conseil Régional et le Comité Régional Olympique et Sportif de Lorraine pour la gestion de l’outil « Maison des Ligues ». Le CROSL est une association financée par le ministère via le CNDS (Centre National de Développement du Sport).
+
+</p>
+</body>
+</html>
