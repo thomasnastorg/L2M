6b65f7bcba9b21a1bf1985236a8ebc3eecc89325
HUET Dolores <dolores.hv3t@gmail.com>
2021-04-28 22:08:26 +0200

    locaux

--- Locaux.php
+++ Locaux.php
@@ -17,9 +17,28 @@ Les étages des bâtiments A et C hébergent les bureaux des ligues locataires e
 
 En fonction de leur taille, les 24 structures hébergées occupent un ou plusieurs bureaux. Au total, la M2L offre 80 bureaux. À chaque étage des bâtiments de bureaux se trouve une petite salle de réunion que les ligues utilisent sasn réservation. L'ensemble des autres salles ressources sont accessibles sur réservation, en journée comme en soirée.
 
-<h2> IPLANTATION DES QUATRES BÂTIMENTS<h2>
+<h2> IPLANTATION DES QUATRES BÂTIMENTS</h2>
 
-<a href=
+<img src="images/sch2.png"/>
+</p>
+
+
+<p>
+
+
+Notes :
+les salles Majorelle, Grüber, Lamour, Longwy, Daum, Gallé, Corbin et Baccarat sont des salles de réunion disponibles à la réservation. La salle multimédia est une salle dédiée aux stages de formation à inscription libre proposés par le CROSL ou pour les stages organisés par les ligues. L'amphithéâtre est réservable pour les assemblées générales ou pour d'autres réunions importantes. La salle de convivialité, et son office attenant, est également disponible à la réservation, souvent pour les repas "traiteur" qui suivent les réunions. Toutes ces salles sont accessibles en soirée : un système de "digicode" permet d'entrer dans les locaux en dehors des heures d'ouverture des bureaux. Différents services peuvent être demandés en parallèle à la réservation : aménagement particulier, fourniture d’eau, de café etc. Ces services sont pour la plupart payants.
+La salle de reprographie est un espace commun aux ligues et à l'administration de la M2L.
+
+Les ligues ont la possibilité de réserver gratuitement l'amphithéâtre ou la salle de convivialité une fois par an et six fois les salles de réunion. Au delà, les réservations sont payantes. D’autres structures extérieures peuvent réserver l’ensemble des salles ressources moyennant finance.
+
+Implantation des ligues dans les bureaux En fonction de leur importance, les ligues et comités départementaux occupent un ou plusieurs bureaux. Ceux-ci peuvent communiquer entre eux quand ils sont contigus. Chaque bureau dispose d'un espace de rangement. Les employés et les bénévoles des ligues et CD disposent en bout d'étage d'une salle de réunion libre, laquelle est accessible seulement en journée et sans réservation (« premier arrivé, premier servi »). La location se fait sur la base d'un forfait de charges locatives par bureau (5€ par m2 et par mois).
+
+</p>
+
+<p>
+<h2> PLAN STANDARD D'ÉTAGE : EXEMPLE DU DEUXIÈME ÉTAGE DU BÂTIMENT A </h2>
+<img src="images/sch3.png"/>
 
 </p>
 </body>
