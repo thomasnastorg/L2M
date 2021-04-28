1be9fc9e0fc4462c28a44d45d5b3fc6a377a883e
Fandresena02 <82474587+Fandresena02@users.noreply.github.com>
2021-04-28 10:42:30 +0200

    Modification de la page connexion
    page fonctionnelle maintenant

--- connexion.php
+++ connexion.php
@@ -1,5 +1,6 @@
 <?php
 include 'db_connect.php';
+include 'header.php';
 session_start();
 $_SESSION["is_loged"]="false";
 $_SESSION["log_nom"]="";
@@ -9,20 +10,38 @@ $_SESSION["log_prenom"]="";
         $login = $_POST["login"];
         $mdp = $_POST["mdp"];
 
-        $req= "select count(*) as nbr, USR_NOM, USR_PRENOM from connexion where login='$login' and mdp='$mdp'";
+        $req= "select count(*) as nbr, usr_nom, usr_prenom from connexion where usr_login='$login' and usr_pass='$mdp'";
         $res = $dbh -> query($req);
         $data = $res -> fetch(PDO::FETCH_ASSOC);
 
     if ($data["nbr"]==1){
         $_SESSION["is_loged"]="true";
-        $_SESSION["log_nom"]=$data["USR_NOM"];
-        $_SESSION["log_prenom"]=$data["USR_PRENOM"];
+        $_SESSION["log_nom"]=$data["usr_nom"];
+        $_SESSION["log_prenom"]=$data["usr_prenom"];
         echo "connexion réussie..."; 
         header("Location: index.php");
         exit;
 
     }
     }
+    if (isset ($_POST["save"])) {
+
+            $usr_nom= isset ($_POST["usr_nom"]) ? $_POST["usr_nom"]:"";
+            $usr_prenom= isset ($_POST["usr_prenom"]) ? $_POST["usr_prenom"]:"";
+            $usr_login= isset ($_POST["usr_login"]) ? $_POST["usr_login"]:"";
+            $usr_pass= isset ($_POST["usr_pass"]) ? $_POST["usr_pass"]:"";
+            $usr_email= isset ($_POST["usr_email"]) ? $_POST["usr_email"]:"";
+            $usr_dep= isset ($_POST["usr_dep"]) ? $_POST["usr_dep"]:"";
+            $usr_ville= isset ($_POST["usr_ville"]) ? $_POST["usr_ville"]:"";
+            $usr_tel= isset ($_POST["usr_tel"]) ? $_POST["usr_tel"]:"";
+            
+
+            $req1 = "insert into utilisateur (usr_nom, usr_prenom, usr_email, usr_login, usr_pass,
+            usr_dep, usr_ville, usr_tel) values ('$usr_nom', '$usr_prenom','$usr_email', '$usr_login', '$usr_pass', '$usr_dep',
+            '$usr_ville', '$usr_tel')";
+            $res1 = $dbh -> query($req1);
+
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -30,22 +49,12 @@ $_SESSION["log_prenom"]="";
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <title>Page de connexion</title>
+   
   </head>
   <body>
   <h1><center>Page de connexion</h1><hr><br>  
-  <form action="connexion.php" method="POST"class="form-horizontal">
+  <form action="index.php" method="POST"class="form-horizontal">
   <div class="form-group">
     <label for="inputEmail3" class="col-sm-2 control-label">Login:</label>
     <div class="col-sm-10">
@@ -69,9 +78,6 @@ $_SESSION["log_prenom"]="";
   </div>
 </form>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+   
   </body>
 </html>
