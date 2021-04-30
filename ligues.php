237ca8b8e19afb690f5bbe7f9b6493c7798a1f38
LeDanNGUYEN <81683941+LeDanNGUYEN@users.noreply.github.com>
2021-04-30 10:20:07 +0200

    ligue_modif
    début mise en place pop-up

---
+++ ligues.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_ligue.css">
+    <script defer src="script.js"></script>
+    <title>Ligue</title>
+</head>
+<body>
+    <h2>Structures hébergées</h2>
+
+    <!-- <div class="grid_ligue"> -->
+        <!-- <div class="ligue"> -->
+            <!-- <img src="./images/Baseball.png" alt="Image baseball" class="image_ligue"> -->
+            <!-- <p>Sport à préciser</p> -->
+            <!-- <button>Plus d'info</button> -->
+            <!-- <a href="#">Plus d'info</a> -->
+        <!-- </div> -->
+    <!-- </div> -->
+
+    <button>Plus d'info</button>
+    <div class="detail_ligue" id="detail_ligue">
+        <div class="detail_ligue_header">
+            <div class="title">Example Ligue</div>
+            <button class="close_button">&times;</button>
+        </div>
+        <div class="detail_ligue_body">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Expedita, voluptatem nostrum! Asperiores repellendus expedita, 
+            necessitatibus ex consequuntur, voluptate suscipit quas officiis magnam voluptatem, 
+            similique aspernatur ducimus temporibus obcaecati amet animi tempora commodi assumenda quaerat. 
+            Enim fugiat ab laudantium error cupiditate ipsa doloremque provident quis modi quod inventore illum qui dolorem, 
+            voluptatibus at ad voluptates. Esse iusto nesciunt fuga dolores, alias, 
+            asperiores eius praesentium voluptatem quisquam laudantium dolorem at nam. 
+            Nostrum corrupti tempora exercitationem fuga, adipisci et animi inventore impedit facere!
+        </div>
+    </div>
+
+    <div id="overlay"></div>
+
+</body>
+</html>
