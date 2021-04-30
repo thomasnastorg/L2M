7261b98874f7ba8b3d40f9bef49a12629de8c09e
LeDanNGUYEN <81683941+LeDanNGUYEN@users.noreply.github.com>
2021-04-30 15:59:48 +0200

    ligue_update
    mise en place du popup et de la grid pour la mise en page
    Fini

--- ld_perso/ligue.php
+++ ld_perso/ligue.php
@@ -13,15 +13,9 @@
     <h2>Structures hébergées</h2>
     
     <section class="basic-grid">
-        <div class="card">
-            <img src="./img_ld/baseball.jpg" alt="match_baseball" width=200px>
-            <p>Sport à préciser</p>
-        </div>
 
-        <div class="card">
-            <img src="./img_ld/baseball.jpg" alt="match_baseball" width=200px>
-            <p>Sport à préciser</p>
-        </div>
+        <!-- Remarque : pour l'ajout d'un nouvelle ligue, copier le bloc ci-dessous
+        ET changer l'id de la div avec classe 'modal' ainsi que data-modal-target avec le même nom -->
 
         <div class="card">
             <img src="./img_ld/baseball.jpg" alt="match_baseball" width=200px>
@@ -34,6 +28,49 @@
                     <button data-close-button class="close-button">&times;</button>
                 </div>
                 <div class="modal-body">
+                    Texte 01 &nbsp;&nbsp;&nbsp;
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                </div>
+            </div>
+            <div id="overlay"></div>
+        </div>
+
+        <div class="card">
+            <img src="./img_ld/baseball.jpg" alt="match_baseball" width=200px>
+            <p>Sport à préciser</p>
+
+            <button data-modal-target="#modal2">Open Modal</button>
+            <div class="modal" id="modal2">
+                <div class=modal-header>
+                    <div class="title">Example Modal</div>
+                    <button data-close-button class="close-button">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Texte 02 &nbsp;&nbsp;&nbsp;
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+                </div>
+            </div>
+            <div id="overlay"></div>
+        </div>
+
+        <div class="card">
+            <img src="./img_ld/baseball.jpg" alt="match_baseball" width=200px>
+            <p>Sport à préciser</p>
+
+            <button data-modal-target="#modal3">Open Modal</button>
+            <div class="modal" id="modal3">
+                <div class=modal-header>
+                    <div class="title">Example Modal</div>
+                    <button data-close-button class="close-button">&times;</button>
+                </div>
+                <div class="modal-body">
+                    Texte 03 &nbsp;&nbsp;&nbsp;
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
