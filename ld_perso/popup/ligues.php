31c71d053d70951f6c02a6503efb06c3f2be163a
LeDanNGUYEN <81683941+LeDanNGUYEN@users.noreply.github.com>
2021-04-30 13:26:52 +0200

    ligue php pop up
    version finie pour pop up
    Passage a la structure en GRID

---
+++ ld_perso/popup/ligues.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_ligue.css">
+    <script defer src="script_ligue.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <button data-modal-target="#modal">Open Modal</button>
+    <div class="modal" id="modal">
+        <div class=modal-header>
+            <div class="title">Example Modal</div>
+            <button data-close-button class="close-button">&times;</button>
+        </div>
+        <div class="modal-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, accusamus?
+        </div>
+    </div>
+
+    <div id="overlay"></div>
+
+
+</body>
+</html>
--- ld_perso/popup/trash/ligues0.php
+++ ld_perso/popup/trash/ligues0.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_ligue.css">
-    <script defer src="script.js"></script>
+    <link rel="stylesheet" href="style_ligue0.css">
+    <script defer src="script0.js"></script>
     <title>Ligue</title>
 </head>
 <body>
@@ -20,13 +20,13 @@
         <!-- </div> -->
     <!-- </div> -->
 
-    <button>Plus d'info</button>
-    <div class="detail_ligue" id="detail_ligue">
-        <div class="detail_ligue_header">
-            <div class="title">Example Ligue</div>
-            <button class="close_button">&times;</button>
+    <button data-modal-target="#modal" >Plus d'info</button>
+    <div class="modal" id="modal">
+        <div class="modal-title">
+            <div class="title">Exemple Ligue</div>
+            <button data-close-button class="close_button">&times;</button>
         </div>
-        <div class="detail_ligue_body">
+        <div class="modal-body">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
             Expedita, voluptatem nostrum! Asperiores repellendus expedita, 
             necessitatibus ex consequuntur, voluptate suscipit quas officiis magnam voluptatem, 
