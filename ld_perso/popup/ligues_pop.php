aaf4b75ef414c8aca7730eb78b3c797333ce11bd
LeDanNGUYEN <81683941+LeDanNGUYEN@users.noreply.github.com>
2021-04-30 14:21:32 +0200

    reorganisation

--- ld_perso/popup/ligues_pop.php
+++ ld_perso/popup/ligues_pop.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_ligue.css">
-    <script defer src="script_ligue.js"></script>
+    <link rel="stylesheet" href="style_ligue_pop.css">
+    <script defer src="script_ligue_pop.js"></script>
     <title>Document</title>
 </head>
 <body>
