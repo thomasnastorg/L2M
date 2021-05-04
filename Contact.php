639f682763b4e87797c7b1c89e60bb8da586cba8
HUET Dolores <dolores.hv3t@gmail.com>
2021-05-04 11:55:30 +0200

    Page contact

--- Contact.php
+++ Contact.php
@@ -1,3 +1,84 @@
 <?php
 include 'header.php';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    
+<center>
+ <!--FAIRE PANNEAU REDUCTIBLE ICI   -->
+<h1>Coordonnées</h1><br><br>
+
+<u>Maison des ligues de lorraine</u><br><br>
+
+13 rue Jean Moulin - BP 70001 54510 TOMBLAINE<br><br>
+
+Tél. : 03.83.18.87.02<br><br>
+
+Fax : 03.83.18.87.03<br><br>
+
+Email : contact@m2l.fr<br><br>
+<br><br><br><br>
+
+<u>DIRECTEUR Lucien SAPIN</u><br><br>
+
+Tél. : 03 83 18 87 02<br><br>
+
+Fax : 03 83 18 87 03<br><br>
+
+Email : lucien.gastaldello@m2l.fr<br><br>
+<br><br><br><br>
+
+<u>CHARGÉE IMPACT EMPLOI</u><br><br>
+
+Nathalie GENAIS<br><br>
+
+Tél. : 03 83 18 87 05<br><br>
+
+Email : martine.genoux@m2l.fr<br><br>
+<br><br><br><br>
+
+<u>SECRÉTAIRE COMPTABLE</u><br><br>
+
+Martine DUPONT<br><br>
+
+Tél. : 03 83 18 87 07<br><br>
+
+Fax : 03 83 18 87 03<br><br>
+
+Email : jeannine.vuillemard@m2l.fr<br><br>
+
+
+<!-- Fénetre réductible -->
+<h1>CONTACTS</h1><br><br>
+
+<u> Nom :</u> <br> <input> 
+<br>
+<u> Prenom :</u> <br> <input> 
+<br><br>
+<u> Email :</u> <br> <input  placeholder="  Votre@mail.fr"> 
+<br><br><br><br><br>
+<u>Message :  </u>    <input>
+
+<input type="submit" value="Envoyé">
+
+
+</center>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
