3604fe52c690d55f04ea7eb09aec77ab3caf655f
MarieRmsy <67025836+MarieRmsy@users.noreply.github.com>
2021-05-06 23:38:22 +0200

    CONNEXION INSCRIPTION CSS

--- connexion.php
+++ connexion.php
@@ -46,43 +46,45 @@ include 'header.php';
 }
 ?>
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Page de connexion</title>
-   
-  </head>
+<title>M2L : Connexion</title>
   <body>
-  <h1><center>Page de connexion</h1><hr><br>  
-  <form action="./connexion.php" method="POST" class="form-horizontal">
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Login:</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" name ="usr_login" placeholder="votre login">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Mot de passe:</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" name="usr_pass" placeholder="votre mot de passe">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <button type="submit" class="btn btn-default" name="connexion">Connexion</button>
-    </div>
-  </div><br><br>
-  <div><center>
-  Pas de compte?
-  <a href="inscription.php"><button type="button" class="btn btn-default">S'inscrire</button></a>
-  </div>
-</form>
-<?php
 
-?>
-   
+<section id="main">
+					<div class="container">
+
+						<!-- Content -->
+							<article class="box post">
+								
+								<header>
+                                    <center><h2 style="margin-bottom: 50px;">Se connecter</h2></center>
+                
+                                    
+                                    
+                                    <form action="./connexion.php" method="POST" class="form-horizontal">
+                                    <div class="form-group">
+                                      <label for="inputEmail3" class="col-sm-2 control-label">Login:</label>
+                                      <div class="col-sm-10">
+                                        <input type="text" class="form-control" name ="usr_login" placeholder="votre login">
+                                      </div>
+                                    </div>
+                                    <div class="form-group">
+                                      <label for="inputPassword3" class="col-sm-2 control-label">Mot de passe:</label>
+                                      <div class="col-sm-10">
+                                        <input type="password" class="form-control" name="usr_pass" placeholder="votre mot de passe">
+                                      </div>
+                                    </div>
+                                    <div class="form-group">
+                                      <div class="col-sm-offset-2 col-sm-10">
+                                        <button type="submit" class="btn btn-default" name="connexion"  style="background-color: #d52349;">Connexion</button>
+                                        <span style="margin-left:20px;">Pas de compte?</span>
+                                      <a href="inscription.php"><button type="button" class="btn btn-default" style="background-color: #d52349;">S'inscrire</button></a>
+                                      </div>
+                                    </div>
+                                  </form>
+                              </header>
+                          </article> 
+					</div>
+				</section>
+
+
   </body>
-</html>
