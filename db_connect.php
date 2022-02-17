859eecc25bd4cc9a25ca6f458fbf969a50d47476
Creepy4Bbby <82474796+Creepy4Bbby@users.noreply.github.com>
2022-02-17 10:21:07 +0100

    db

--- db_connect.php
+++ db_connect.php
@@ -2,6 +2,6 @@
 $user = "MDL";
 $mdp = "LNfqq93fT";
 
-$dbh = new PDO("mysql:host=tnastorg.ovh;dbname=MDeuxL",$user,$mdp);
+$dbh = new PDO("mysql:host=46.105.30.38;dbname=MDeuxL",$user,$mdp);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
